90951e4e57bd143302bb33a38ae5bf9a22d937b8
Mark Tasaka <mark.tasaka@gmail.com>
2020-05-25 21:29:43 -0700

    Update 1.01

---
+++ tavernGen/tavern.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tavern Generator</title>
+ 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+	<meta charset="UTF-8">
+	<meta name="description" content="Dungeon Crawl Classics tavern Character Generator. Goodman Games.">
+	<meta name="keywords" content="Dungeon Crawl Classics, Goodman Games,HTML5,CSS,JavaScript">
+	<meta name="author" content="Mark Tasaka 2020">
+    
+    <link rel="icon" href="../../../images/favicon/icon.png" type="image/png" sizes="16x16"> 
+		
+	<link rel="stylesheet" type="text/css" href="css/tavern.css">
+    
+</head>
+<body>
+<?php
+
+
+
+
+
+?>
+
+<img id="character_sheet"/>
+
+<script>
+    
+    let imgData = "images/tavern1.png";
+      
+      $("#character_sheet").attr("src", imgData);
+
+</script>
+
+</body>
+</html>
